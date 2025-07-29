f4521d21d8c00933b29f7981d60e78356252a1b5
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-30 00:17:43 +0530

    Create Print SIT 5 times.cpp

---
+++ Print SIT 5 times.cpp
@@ -0,0 +1,26 @@
+//Name-Sanskriti Khairnar
+//PRN - 24070123054
+//Batch - A2
+//Experiment 6
+
+//Program 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0 ; i<5 ; i++)
+        {
+            cout<<"SIT"<<endl;
+        }
+
+    return 0;
+}
+
+//Output
+
+//SIT
+//SIT
+//SIT
+//SIT
+//SIT
