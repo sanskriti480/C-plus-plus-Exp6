b7c748f5dd487fdfe0ebd788fd9e48ae24c3a24a
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-30 00:15:40 +0530

    Create Print even numbers.cpp

---
+++ Print even numbers.cpp
@@ -0,0 +1,27 @@
+//Name-Sanskriti Khairnar
+//PRN - 24070123054
+//Batch - A2
+//Experiment 6
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0 ; i<=10 ; i++){
+        if(i%2 == 0)
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
+
+
+//Outputs 
+
+//0
+//2
+//4
+//6
+//8
+//10
